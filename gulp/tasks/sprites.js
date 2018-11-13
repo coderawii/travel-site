4094b1312a56893fe7b98fb46b30f53d8a47758d
coderawii <coderawii@gmail.com>
2018-11-13 16:56:46 +0100

    035 Automatic sprite generation with gulp III - END. task: beginClean, endedClean tasks - about del/add icons

--- gulp/tasks/sprites.js
+++ gulp/tasks/sprites.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp'),
 svgSprite = require('gulp-svg-sprite'),
-rename = require('gulp-rename');
+rename = require('gulp-rename'),
+del = require('del');
 
 var config = {
     mode: {
@@ -15,7 +16,11 @@ var config = {
     }
 }
 
-gulp.task('createSprite', function(){
+gulp.task('beginClean', function() {
+    return del(['./app/temp/sprite', './app/assets/images/srpites']);
+});
+
+gulp.task('createSprite', ['beginClean'], function(){
     return gulp.src('./app/assets/images/icons/**/*.svg')
         .pipe(svgSprite(config))
         .pipe(gulp.dest('./app/temp/sprite/'));
@@ -24,7 +29,7 @@ gulp.task('createSprite', function(){
 gulp.task('copySpriteGraphic', ['createSprite'],  function() { // ovo ['createSrpite'] ubaceno ovde znaci da je on dependency ovom tasku, tj copySpriteGraphic zavisi od createSprite 
     return gulp.src('./app/temp/sprite/css/**/*.svg')
         .pipe(gulp.dest('./app/assets/images/sprites'));
-})
+});
 
 gulp.task('copySpriteCss', ['createSprite'], function(){ // ovo ['createSprite'] smo dodali da se ovaj task copySpriteCss ne krene izvrsavati pre nego se ovaj createSprite def ne zavrsi/izvrsi
     return gulp.src('./app/temp/sprite/css/*.css')
@@ -32,6 +37,9 @@ gulp.task('copySpriteCss', ['createSprite'], function(){ // ovo ['createSprite']
         .pipe(gulp.dest('./app/assets/styles/modules'));
 });
 
-// bilo bi idealno kada bi umesto ova 2 taska pokretali zapravo samo jedan koji sadrzi oba ova taska
+gulp.task('endClean', ['copySpriteGraphic', 'copySpriteCss'], function() {
+    return del('./app/temp/sprite');
+});
 
-gulp.task('icons', ['createSprite', 'copySpriteCss', 'copySpriteGraphic']); // dakle prvo ce da se izvrsava createSprite, i onda ce ovaj copySpriteCss da ceka sve dok se ovaj createSprite ne izvrsi
+// bilo bi idealno kada bi umesto ova 2 taska pokretali zapravo samo jedan koji sadrzi oba ova taska
+gulp.task('icons', ['beginClean', 'createSprite', 'copySpriteCss', 'copySpriteGraphic', 'endClean']); // dakle prvo ce da se izvrsava createSprite, i onda ce ovaj copySpriteCss da ceka sve dok se ovaj createSprite ne izvrsi
