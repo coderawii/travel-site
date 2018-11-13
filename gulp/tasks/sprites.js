4281cd117741492d8cbf27d0a19f91a3d7835f23
coderawii <coderawii@gmail.com>
2018-11-13 16:12:52 +0100

    034 Automatic Sprites with Gulp II, pravljnje _sprite.css u modules preko gulpa, novi taskovi icons,tj spajanje vise taskova u ovaj jedan, copySpriteGraphic..

--- gulp/tasks/sprites.js
+++ gulp/tasks/sprites.js
@@ -1,13 +1,15 @@
 var gulp = require('gulp'),
-svgSprite = require('gulp-svg-sprite');
+svgSprite = require('gulp-svg-sprite'),
+rename = require('gulp-rename');
 
 var config = {
     mode: {
         css: {
-           render: {
-               css: {
-                   template: './gulp/templates/sprite.css'
-               }
+            sprite: 'sprite.svg', // ovim uklanjamo .css iz file imena za background-image: url('/temp/sprite/css/svg/sprite-69f19c2e.svg'); (dakle bilo je sprite.css-......)
+            render: {
+                css: {
+                    template: './gulp/templates/sprite.css'
+                }
            } 
         }
     }
@@ -18,3 +20,18 @@ gulp.task('createSprite', function(){
         .pipe(svgSprite(config))
         .pipe(gulp.dest('./app/temp/sprite/'));
 });
+
+gulp.task('copySpriteGraphic', ['createSprite'],  function() { // ovo ['createSrpite'] ubaceno ovde znaci da je on dependency ovom tasku, tj copySpriteGraphic zavisi od createSprite 
+    return gulp.src('./app/temp/sprite/css/**/*.svg')
+        .pipe(gulp.dest('./app/assets/images/sprites'));
+})
+
+gulp.task('copySpriteCss', ['createSprite'], function(){ // ovo ['createSprite'] smo dodali da se ovaj task copySpriteCss ne krene izvrsavati pre nego se ovaj createSprite def ne zavrsi/izvrsi
+    return gulp.src('./app/temp/sprite/css/*.css')
+        .pipe(rename('_sprite.css'))
+        .pipe(gulp.dest('./app/assets/styles/modules'));
+});
+
+// bilo bi idealno kada bi umesto ova 2 taska pokretali zapravo samo jedan koji sadrzi oba ova taska
+
+gulp.task('icons', ['createSprite', 'copySpriteCss', 'copySpriteGraphic']); // dakle prvo ce da se izvrsava createSprite, i onda ce ovaj copySpriteCss da ceka sve dok se ovaj createSprite ne izvrsi
