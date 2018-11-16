82b35e7af1bcb82e5177e4c962e9d7ee5a3caf4e
coderawii <coderawii@gmail.com>
2018-11-16 17:15:01 +0100

    044 - dovrsavanje Mobile Menu, css. - display zamenjen sa opacity-em zbog transform efekata, samim tim dodat z-index jer je za opacity sadrzaj i dalje tu, samo nije vidljiv, dok sa display nije tu ili jeste. A zbog z-indexa, dodajemo i position, i pisanimo style za header i za mobile i za velike ekrane

--- app/assets/scripts/modules/MobileMenu.js
+++ app/assets/scripts/modules/MobileMenu.js
@@ -6,6 +6,7 @@ class MobileMenu {
         //     console.log("Gornj desna ikonica je kliknuta, ali ovo je nnapravlejno jquery spagetty kodom");
         // }); // jquery spaghetti jer se radi sve odjednom, prvo selektujemo elemente iz DOMa, drugo mi rukujemo s dogadjajima, i trece definisemo fukncionalnost, tj sta treba da se uradi odredjenim eventom (selektor-event-funckionalnost iliti forks-spones-knifes iliti viljuske-kasike-nozevi) 
 
+        this.siteHeader = $(".site-header");
         this.menuIcon = $(".site-header__menu-icon");
         this.menuContent = $(".site-header__menu-content");
         this.events();
@@ -26,6 +27,8 @@ class MobileMenu {
         // alert(this);
 
         this.menuContent.toggleClass("site-header__menu-content--is-visible"); // App.js:145 Uncaught TypeError: Cannot read property 'toogleClass' of undefined - i ova greska je sve zbog js THIS keyworda, ovu gresku dobijamo jer ovaj kod (this.menuContent) ne pristupa uspesno menuContent propertyju, i onda se pitamo zasto recimo u events() this.menuIcon ili this.toggleMenu f-nise kako treba, ili u constructoru u this.events(), a ovde ne. Odg je, jer se vrednost this-a menja u zavisnosti gde ga koristimo, u normalnim uslovima, (ovim prethodno navedenim gde recimo radi) kada this koristimo u sklopu/unutar objekata on ukazuje na objekat, ali zasto nam ovde this ima drugaciji value, zasto ne pokazuje na objekat? zato sto kada se ovaj toggleTheMenu metod pokrene, mi ga nismo pozvali direktno vec preko klika, kada korisnik klikne, dakle pozivamo ovaj metod u events() tj u klik event. A kad se f-ja koristi kao event hendler, this keyword unutar te fje (dakle this u toggleTheMenu), setuje DOM element da bude onaj gde je event okinut, dakle u ovom slucaju element na koji je kliknuto sto bi bio nas menuIcon element. I to mozemo bolje prikazati tako sto cemo metnuti console.log(this) u oba ova metoda, i y events() i u toggleTheMenu() da vidimo na sta ce da ukazuju
+
+        this.siteHeader.toggleClass("site-header--is-expanded");
     }
 }
 
