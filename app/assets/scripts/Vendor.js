9c91f1eafc0978184f6aa90668ecb5aac23c04c4
coderawii <coderawii@gmail.com>
2018-11-19 22:33:51 +0100

    054 Support for Responsive Images in Legacy Browsers - nista, samo smo instalirali npm install picturefill --save, i u Vendor.js ubacili import picturefill i ovim sada se slike valjda ucitavaju (one response picture, srcset) i u starim browserima

--- app/assets/scripts/Vendor.js
+++ app/assets/scripts/Vendor.js
@@ -1,3 +1,5 @@
+import 'picturefill';
+
 // kada su neki packages u pitanju onda je bolje da se napravi poseban js file za njihov import, a ne u recimo App.js, iu zato naapravismo Vendor.js
 import 'lazysizes';
 // Vendor.js smo ucitali u head sekciju, a ne dole iznad </body>, potom smo za testimonials slike, tj img, za klasu stavili lazysizes-klasu lazyload, a za srcset stavili data-srcset, i sada se ove testimonials slike dowmloaduju tek kad se skroluje neposredno blizu mesta na kojima su. A za backgroudn of testimonials koji smo ucitali iz css-a, u index.html dodajemo klasu gde je ucitana ta slika lazyload, a potom u css-u ono gde je ucitana ta slika premestamo u klasu .lazyloaded
